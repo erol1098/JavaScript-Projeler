b41e12b9e5b3473a720edf5e5872a43b875554de
erol1098 <erol1098@gmail.com>
2022-04-03 13:07:19 +0300

    Code Dry

--- 002-PigGame/script.js
+++ 002-PigGame/script.js
@@ -10,7 +10,6 @@ const player2Current = document.querySelector('#current--1');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
-
 // Inıtıal Values
 let counter = 0;
 const players = [
@@ -19,40 +18,41 @@ const players = [
 ];
 let currentPlayer = players[counter % 2];
 let gameEnd = false;
-
+let currentScore = 0;
+let totalScore = 0;
 // Rolling Dice Function
 const rollingDice = function () {
   if (!gameEnd) {
     let number = Math.trunc(((Math.random() * 10) % 6) + 1);
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${number}.png`;
-    currentPlayer[1].textContent =
-      Number(currentPlayer[1].textContent) + number;
+    currentScore = Number(currentPlayer[1].textContent);
+    currentScore += number;
+    currentPlayer[1].textContent = currentScore;
     if (number === 1) {
       currentPlayer[1].textContent = 0;
+      currentScore = 0;
       switchPlayer();
     }
   }
 };
-
 // Hold Score Function
 const holdScore = function () {
   if (!gameEnd) {
-    currentPlayer[0].textContent =
-      Number(currentPlayer[0].textContent) +
-      Number(currentPlayer[1].textContent);
+    totalScore = Number(currentPlayer[0].textContent);
+    totalScore += currentScore;
+    currentPlayer[0].textContent = totalScore;
+    currentScore = 0;
     currentPlayer[1].textContent = 0;
     // Winner
-    if (Number(currentPlayer[0].textContent) >= 10) {
+    if (Number(currentPlayer[0].textContent) >= 100) {
       currentPlayer[2].classList.add('player--winner');
       diceEl.classList.add('hidden');
       gameEnd = true;
     }
-
     switchPlayer();
   }
 };
-
 // Switch Player
 const switchPlayer = function () {
   counter++;
@@ -60,7 +60,6 @@ const switchPlayer = function () {
   player1.classList.toggle('player--active');
   player2.classList.toggle('player--active');
 };
-
 // New Game Function
 // Starting condition fuction
 const newGame = function () {
@@ -74,12 +73,14 @@ const newGame = function () {
   player1.classList.add('player--active');
   player2.classList.remove('player--active');
   counter = 0;
+  gameEnd = false;
+  currentPlayer = players[counter % 2];
+  gameEnd = false;
+  currentScore = 0;
+  totalScore = 0;
 };
-
 // Button Events
-
 btnRoll.addEventListener('click', rollingDice);
 btnHold.addEventListener('click', holdScore);
 btnNew.addEventListener('click', newGame);
-
 newGame();
